be8762ee60cf8ebff3791369ea55c4432181b28f
Saksham <sakshamverma7844@gmail.com>
2023-10-21 20:51:02 +0530

    done

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,7 +4,7 @@ import { createProjectMutation, createUserMutation, deleteProjectMutation, updat
 import { ProjectForm } from "@/common.types";
 
 const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || 'http://127.0.0.1:4000/graphql' : 'http://127.0.0.1:4000/graphql';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
